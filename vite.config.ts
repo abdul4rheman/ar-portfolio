9c3b65bd275b0ee2a4772e5692c69630c88bddd4
Shaikh Abdul Rheman Mohsin <mrabdul4rheman@gmail.com>
2025-07-02 17:05:55 +0000

    updated vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  base: 'https://www.abdul4rheman.me/',
   plugins: [
     react(),
     mode === 'development' &&
